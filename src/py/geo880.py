b8e17d3c3357204b430c94abfeb754ea160b65e5
Robin Jia <robinjia@stanford.edu>
2015-11-11 10:43:46 -0800

    Stop copying predicates in geoquery, minor tokenization change

--- src/py/geo880.py
+++ src/py/geo880.py
@@ -1,6 +1,7 @@
 """Some code to deal with geo880 data."""
 import collections
 import glob
+from nltk.stem.porter import PorterStemmer
 import os
 import re
 import sys
@@ -11,6 +12,12 @@ IN_DIR = os.path.join(
 OUT_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
     'data/geo880/processed')
+STEM_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+    'data/geo880/processed-stems')
+LESS_COPY_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+    'data/geo880/processed-lesscopy')
 
 def read_examples(filename):
   examples = []
@@ -33,12 +40,26 @@ def split_logical_form(lf):
       (')', ' ) '),
       (',', ' , '),
       ("'", " ' "),
+      ("\\+", " \\+ "),
   ]
   for a, b in replacements:
     lf = lf.replace(a, b)
   return ' '.join(lf.split())
 
-def process(filename):
+def reduce_copying(lf):
+  # List all predicates (whitelist)
+  PREDS = [
+      'cityid', 'countryid', 'placeid', 'riverid', 'stateid',
+      'capital', 'city', 'lake', 'major', 'mountain', 'place', 'river',
+      'state', 'area', 'const', 'density', 'elevation', 'high_point',
+      'higher', 'loc', 'longer', 'low_point', 'lower', 'len', 'next_to',
+      'population', 'size', 'traverse',
+      'answer', 'largest', 'smallest', 'highest', 'lowest', 'longest',
+      'shortest', 'count', 'most', 'fewest', 'sum']
+  toks = ['_' + w if w in PREDS else w for w in lf.split()]
+  return ' '.join(toks)
+
+def process(filename, stemmer=None, less_copy=False):
   print >> sys.stderr, 'Processing %s' % filename
   basename = os.path.basename(filename)
   domain = basename.split('_')[0]
@@ -47,19 +68,36 @@ def process(filename):
   in_data = read_examples(filename)
   out_data = []
   for (utterance, logical_form) in in_data:
+    if stemmer:
+      utterance = ' '.join(stemmer.stem(w) for w in utterance.split())
     y = split_logical_form(logical_form)
+    if less_copy:
+      y = reduce_copying(y)
     out_data.append((utterance, y))
 
   out_basename = '%s_%s.tsv' % (domain, stage)
-  with open(os.path.join(OUT_DIR, out_basename), 'w') as f:
+  if stemmer:
+    out_path = os.path.join(STEM_DIR, out_basename)
+  elif less_copy:
+    out_path = os.path.join(LESS_COPY_DIR, out_basename)
+  else:
+    out_path = os.path.join(OUT_DIR, out_basename)
+  with open(out_path, 'w') as f:
     for x, y in out_data:
       print >> f, '%s\t%s' % (x, y)
 
 def main():
   if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
+  if not os.path.exists(STEM_DIR):
+    os.makedirs(STEM_DIR)
+  if not os.path.exists(LESS_COPY_DIR):
+    os.makedirs(LESS_COPY_DIR)
+  stemmer = PorterStemmer()
   for filename in sorted(glob.glob(os.path.join(IN_DIR, '*.examples'))):
     process(filename)
+    process(filename, less_copy=True)
+    process(filename, stemmer=stemmer)
 
 if __name__ == '__main__':
   main()
