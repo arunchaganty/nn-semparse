bf17c44dafcd05ab0c65b0763b869b4f4d148d99
Robin Jia <robinjia@stanford.edu>
2016-01-05 13:15:02 -0800

    Fix some paper comments

--- tex/fig-artificial-aug.R
+++ tex/fig-artificial-aug.R
@@ -1,19 +1,26 @@
 # Make plot about artificial data
-pdf('fig-artificial-aug.pdf', width=6, height=6, pagecentre=T)
+pdf('fig-artificial-aug.pdf', width=5, height=5, pagecentre=T)
 par(family='serif')
 x = c(0, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500)
-y_entities = c(40.6, 65.8, 76.6, 56.8, 79.8, 82.0, 92.0, 88.4, 89.6, 92.8, 91.6)
-y_nesting = c(40.6, 80.6, 20.9, 92.4, 80.8, 95.8, 95.0, 79.0, 95.8, 99.8, 82.0)
-y_both = c(40.6, 89.6, 85.6, 99.6, 99.6, 94.2, 100, 94.8, 99.4, 100, 100)
+y_entities = c(6.2, 29.4, 20.8, 54.8, 58.4, 61.4, 66.6, 72.8, 68.8, 74.0, 77.4)
+y_nesting = c(6.2, 73.8, 84.8, 78.6, 87.4, 84.8, 88.8, 93.4, 68.8, 95.4, 80.4)
+y_both = c(6.2, 82.0, 83.8, 72.0, 92.2, 89.6, 93.2, 98.8, 93.6, 95.2, 97.4)
+y_more_depth2 = c(6.2, 28.6, 74.6, 87.2, 89.0, 99.6, 100, 99.8, 100, 100, 100)
+y_more_depth4 = c(6.2, 84.8, 95.2, 98.8, 100, 100, 100, 99.4, 100, 100, 100)
 
-y_min = min(c(min(y_entities), min(y_nesting), min(y_both)))
-plot(x, y_entities, col='black', ylim=c(y_min - 2, 100), type='l',
-     xlab='Number of synthetic examples',
+#y_min = min(c(min(y_entities), min(y_nesting), min(y_both)))
+y_min = 0
+plot(x, y_more_depth2, col='black', ylim=c(y_min - 2, 100), type='l',
+     xlab='Number of additional examples',
      ylab='Accuracy (%)',
-     cex=1.5, cex.axis=1.5, cex.lab=1.5) 
-lines(x, y_nesting, col='blue')
-lines(x, y_both, col='red')
-  legend('bottomright', 
-         c('Entity-based', 'Nesting-based', 'Both'),
-         col=c('black', 'blue', 'red'), lty=1, cex=1.5)
+     #cex=1.5, cex.axis=1.5, cex.lab=1.5) 
+     )
+lines(x, y_more_depth4, col='red', lty='dashed')
+lines(x, y_entities, col='green', lty='dotdash')
+lines(x, y_nesting, col='blue', lty='dotted')
+lines(x, y_both, col='purple', lty='twodash')
+legend('bottomright', 
+       c('depth-2', 'depth-4', 'entity-based', 'nesting-based', 'both'),
+       col=c('black', 'red', 'green', 'blue', 'purple'),
+       lty=c('solid', 'dashed', 'dotdash', 'dotted', 'twodash'))
 dev.off()
