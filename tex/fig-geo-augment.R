ae794f83aaa5a81181ac0f9bbd32febe1496ed16
Robin Jia <robinjia@stanford.edu>
2016-01-06 13:15:42 -0800

    Small paper updates

--- tex/fig-geo-augment.R
+++ tex/fig-geo-augment.R
@@ -2,8 +2,8 @@
 pdf('fig-geo-augment.pdf')
 op = par(family='serif')
 x = c(100, 200, 300, 400, 500, 600)
-y_orig = c(42.9, 60.4, 74.3, 78.5, 82.1, 85.4)
-y_aug = c(50.7, 73.9, 80.0, 81.1, 87.1, 87.9)
+y_orig = c(43.2, 63.2, 75.4, 78.6, 83.9, 85.7)
+y_aug = c(46.8, 68.2, 78.6, 85.0, 85.4, 87.9)
 plot(x, y_orig, ylim=c(35, 90), col='red', 
      xlab='Number of original training examples', ylab='Accuracy (%)',
      cex=1.5, cex.axis=1.5, cex.lab=1.5)
@@ -14,4 +14,3 @@ lines(x, y_aug, col='blue')
 legend('bottomright', c('No augmented data', 'With augmented data'),
        col= c('red', 'blue'), lty=1, cex=1.5)
 dev.off()
-par(op)
